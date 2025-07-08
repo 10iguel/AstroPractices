ffc2a9af20836d3404e3722d31c6dd966f3606eb
Miguel Espinoza <68973067+10iguel@users.noreply.github.com>
2025-07-08 13:29:58 -0600

    - More tabs finished with more texts

--- src/const/faqs.ts
+++ src/const/faqs.ts
@@ -2,11 +2,39 @@ import type {Faqs} from "../types/Faqs.ts";
 
 export const FAQS: Faqs[] = [
     {
-        question:"Who is the cutest",
-        answer:"Of Course, Celeste"
+        question:"How did we meet?",
+        answer:"We got set up and we coordinate a date. Celeste came all the way up to Layton."
     },
     {
-        question:"Who gave the first step",
-        answer:"Of Course, Celeste"
+        question:"Who is the better organizer?",
+        answer:"Celeste is the most organized i have ever known ",
+    },{
+        question:"Who is the morning person?",
+        answer:"That would be Miguel, Celeste likes to snuggle a lot."
+    },
+    {
+        question:"Who is the better driver?",
+        answer:"It was Celeste, but Miguel has stolen the stirring wheel, So Miguel is the better driver"
+    },{
+        question:"Who is better at games?",
+        answer:"In terms of audacity, Miguel is better. But in general, Celeste beats Miguel"
+    },
+    {
+        question:"Who eats more snacks in the house?",
+        answer:"I think this is a Tie"
+    },{
+        question:"Who is the best cook",
+        answer:"We both cook delicious! Celeste super beats me in baking"
+    },
+    {
+        question:"Who is the bigger plant lover?",
+        answer:"Celeste of course. If you come to our house, you will understand "
+    },{
+        question:"Who is the better note-taker",
+        answer:"That's Celeste again. She is trying to make Miguel to write more too"
+    },
+    {
+        question:"Who is the bigger book nerd",
+        answer:"Celeste again, but Miguel is trying hard to read more books"
     },
 ]
--- src/const/importantEvents.ts
+++ src/const/importantEvents.ts
@@ -20,7 +20,7 @@ export const IMPORTANT_EVENTS: ImportantEvents[] = [
         datetime: "2023-06-14T17:00",
         location: "Provo, Utah",
         address: "352 900 E, Provo, UT 84606",
-        description: "Lamb of God in Spanish. We had indian food after the concert and then I went to play soccer"
+        description: "Celeste drove all the way up to Layton to pick me up and then we went to see the Lamb of God in Spanish. We had indian food after it, we had lots of leftovers and then I went to play soccer "
     },
     {
         date: "FRIDAY, JULY 21, 2023",
@@ -34,7 +34,7 @@ export const IMPORTANT_EVENTS: ImportantEvents[] = [
         datetime: "2023-07-21T17:30",
         location: "Timpanogos Cave National Monument",
         address: "2038 E Alpine Lp Rd, American Fork, UT 84003",
-        description: "Anxiously waiting for this day. We gave our first kiss and celebrated with Cafe Rio. Maybe not in that order"
+        description: "After dating for a while, Celeste accepted my proposal by text. We hiked Timpanogos Caves holding our hands the whole time. By the end, We ate Cafe Rio next to the river and We had our first kiss"
     },
     {
         date: "MONDAY, APRIL 29, 2024",
@@ -48,7 +48,7 @@ export const IMPORTANT_EVENTS: ImportantEvents[] = [
         datetime: "2024-04-29T10:00",
         location: "Layton Utah Temple",
         address: "1400 Oak Hills Dr, Layton, UT 84040",
-        description: "After dating for almost a year, we visited the temple to talk about marriage. Celeste took 3 hours to answer but at the end, she accepted my proposal to get married. We celebrated with Cafe rio again."
+        description: "After almost a year of being official, We visited the Layton temple to talk about marriage. Celeste took 3 hours to answer. In front of the Temple, She ended up saying YES!!!. We celebrated with Cafe rio again obviously."
     },
     {
         date: "THURSDAY, MAY 01, 2024",
@@ -62,7 +62,7 @@ export const IMPORTANT_EVENTS: ImportantEvents[] = [
         datetime: "2023-07-21T17:30",
         location: "Timpanogos Cave National Monument",
         address: "2038 E Alpine Lp Rd, American Fork, UT 84003",
-        description: "Our duck activity that lead to one of the most important questions with the most beautiful YES"
+        description: "Miguel hid a secret message for a long time, Celeste had to go to the same place we had our first kiss. We did a duck activity that lead to one of the most important questions with the most beautiful YES"
     },
     {
         date: "FRIDAY, JULY 25, 2024",
@@ -76,7 +76,7 @@ export const IMPORTANT_EVENTS: ImportantEvents[] = [
         datetime: "2023-07-21T17:30",
         location: "Layton Utah Temple",
         address: "1400 Oak Hills Dr, Layton, UT 84040",
-        description: "The most beautiful day in my life was looking at you as my wife and making a covenant before God that I will be with you forever"
+        description: "The most beautiful day in my life was looking at Celeste as my wife and making covenants with her before God that I will be with Celeste forever"
     },
     {
         date: "FRIDAY, JULY 25, 2024",
@@ -90,7 +90,7 @@ export const IMPORTANT_EVENTS: ImportantEvents[] = [
         datetime: "2023-07-21T17:30",
         location: "Big Willow Bar",
         address: "535 N State St, Lindon, UT 84042",
-        description: "Beautiful decoration, delicious food, lots of friends and family together.  "
+        description: "Lots of memories, beautiful decoration, delicious food, the best cake ever, lots of friends and family together. We missed some things but it was a special day to celebrate. At the end of the reception, We locked ourselves out of to the house but we were able to go to the airport at the end"
     },
     {
         date: "SATURDAY, AUGUST 03, 2024",
@@ -104,6 +104,6 @@ export const IMPORTANT_EVENTS: ImportantEvents[] = [
         datetime: "2023-07-21T17:30",
         location: "La Molina Stake",
         address: "Av. Melgarejo 161, La Molina 15026, Peru",
-        description: "After a good nap after our trip to Cuzco. We danced, eat and enjoyed time with my peruvian family and friends"
+        description: "Right after our honeymoon to Cuzco, we took a long nap. We were surprised by the decoration. We danced, eat and enjoyed time with my family and friends."
     },
 ];
