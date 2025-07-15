f97d79ceaa2a451ae971c8bab9295e1cb4f1ba62
Miguel Espinoza <68973067+10iguel@users.noreply.github.com>
2025-07-15 11:15:01 -0600

    - Ready to fix the games and almost finished

--- src/const/routes.ts
+++ src/const/routes.ts
@@ -2,9 +2,8 @@ import type {Routes} from "../types/routes.ts";
 
 export const ROUTES: Routes[] = [
     { path: '/', label: 'Home' },
-    { path: '/schedule', label: 'Schedule' },
+    { path: '/schedule', label: 'TimeLine' },
     { path: '/wedding-party', label: 'Wedding Party' },
-    { path: '/gallery', label: 'Gallery' },
     { path: '/faqs', label: 'FAQs' },
     { path: '/story', label: 'Story' },
     { path: '/things-to-do', label: 'Things To Do' },
